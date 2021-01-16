8dd601d2fa122fab4eb032bcae6c716ec1a3e366
kasinath777 <74859052+kasinath777@users.noreply.github.com>
2021-01-16 14:09:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var canvas;
 var music;
 var block1,block2,block3,block4,ball,obj1,obj2;
+var edges;
 
 function preload(){
     music = loadSound("music.mp3");
@@ -11,16 +12,16 @@ function setup(){
     canvas = createCanvas(800,600);
 
     //create 4 different surfaces
-  block1=createSprite(200,50,200,550);
-  block1.shapeColor="orange"
-  block2=createSprite(200,50,400,550);
-  block2.shapeColor="blue"
-  block3=createSprite(200,50,600,550);
-  block3.shapeColor="green"
-  block4=createSprite(200,50,800,550);
-  block4.shapeColor="pink"
+  block1=createSprite(100,550,100,10);
+  block1.shapeColor=(80,0,0)
+  block2=createSprite(200,550,100,10);
+  block2.shapeColor=rgb(0,0,100
+  block3=createSprite(300,550,100,10);
+  block3.shapeColor=rgb(50,90,150)
+  block4=createSprite(400,550,10,10);
+  block4.shapeColor=rgb(100,50,40
   ball=createSprite(random(20,750),200,10,10);
-  ball.shapeColor="orange"
+  ball.shapeColor=rgb(0,90,0)
   ball.velocityY=random(7,20);
   ball.velocityX=random(7,20);
     //create box sprite and give velocity
@@ -29,15 +30,16 @@ function setup(){
 
 function draw() {
     background(rgb(169,169,169));
-    createEdgeSprites();
+     edges =createEdgeSprites();
+    ball.bounceOff(edges)
     ball.bounceOff(Edges);
     ball.bounceOff(block1);
     ball.bounceOff(block2);
     ball.bounceOff(block3);
     ball.bounceOff(block4);
     if(ball.isTouching(block4)){
-      ball.shapeColor=block4.shapeColor
-      music.play("music.mp3")
+      ball.shapeColor=rgb(100,50,40)
+      music.play();
     }    
 
     
